c7251de588341cc12a340f4a7068c5c89a2e2247
Stefano Misuraca <stefano.misuraca92@gmail.com>
2022-04-10 15:03:45 +0200

    db schemas

--- api/db.js
+++ api/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-export default function dbConnect() {
+export function dbConnect() {
     
-    mongoose.connect('mongodb://mongodb:27017',
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    );
+  mongoose.connect('mongodb://mongodb:27017/superb',
+      {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      }
+  );
   
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error: "));
@@ -16,3 +16,48 @@ export default function dbConnect() {
     return db;
   });
 }
+
+
+const tableSchema = new mongoose.Schema({
+  restaurantId: {
+    type: mongoose.ObjectId,
+    required: true
+  },
+  seats: {
+    type: Number,
+    min: 1,
+    required: true
+  },
+  reservedAt: {
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date,
+      required: true
+    }
+  }
+})
+
+export const Table = mongoose.model("Table", tableSchema);
+
+const restaurantSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    workingHours: {
+      opening: {
+        type: Date,
+        required: true,
+      },
+      closing: {
+        type: Date,
+        required: true,
+      },
+    }
+  });
+
+export const Restaurant = mongoose.model("Restaurant", restaurantSchema);
